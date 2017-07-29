50fc129446b1a032b6cb8b31d968501e08580bb7
Uli Kusterer <ulivc@zathras.de>
2017-07-29 18:26:42 +0200

    Initial Commit

---
+++ ocimages/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocimages
+//
+//  Created by Uli Kusterer on 29.07.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
